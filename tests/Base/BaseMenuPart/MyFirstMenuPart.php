ed173e9df88746f8a120f22fe71e5ae8ebc10301
Meritoo <github@meritoo.pl>
2019-04-16 21:03:54 +0200

    Source code and tests

---
+++ tests/Base/BaseMenuPart/MyFirstMenuPart.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * (c) Meritoo.pl, http://www.meritoo.pl
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Meritoo\Test\Menu\Base\BaseMenuPart;
+
+use Meritoo\Common\Collection\Templates;
+use Meritoo\Menu\Base\BaseMenuPart;
+
+/**
+ * Part of menu used by test case of BaseMenuPart
+ *
+ * @author    Meritoo <github@meritoo.pl>
+ * @copyright Meritoo <http://www.meritoo.pl>
+ *
+ * @internal
+ * @coversNothing
+ */
+class MyFirstMenuPart extends BaseMenuPart
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Class constructor
+     *
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function prepareTemplateValues(Templates $templates): array
+    {
+        return [
+            'name' => $this->name,
+        ];
+    }
+}
